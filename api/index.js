55e74778cb75ef5f0f0ff26f89ada0d7a90c51ef
DarkeNima <naviyaoficial@gmail.com>
2026-03-19 11:38:42 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,27 +1,21 @@
 export default function handler(req, res) {
-  // ගේම් එකෙන් එන විස්තර සර්වර් එකේ ලොග් කරනවා
-  console.log("--- New Request Received ---");
-  console.log("Method:", req.method);
-  console.log("Path:", req.url);
-  console.log("User-Agent:", req.headers['user-agent']);
-  console.log("Headers:", JSON.stringify(req.headers));
+  // සර්වර් එකට Request එකක් එනවාද කියලා බලන්න
+  console.log("Game connecting from:", req.headers['user-agent']);
 
   const responseData = {
     "is_server_open": true,
     "code": 2,
     "country_code": "IN",
     "server_url": "https://ffsverprivate.vercel.app/",
-    "latest_release_version": "",
-    "remote_version": "",
-    "cdn_url": "",
-    "appstore_url": "http://www.freefiremobile.com/",
+    "latest_release_version": "9.9.9", // ලොකු අංකයක් දෙන්න
+    "remote_version": "9.9.9",         // ලොකු අංකයක් දෙන්න
+    "cdn_url": "https://ffsverprivate.vercel.app/",
+    "appstore_url": "https://ffsverprivate.vercel.app/",
     "is_firewall_open": false,
-    "gamevar": "var_name,comment,var_type,var_value\nANODisabledRegions,MTP,string,\"ALL\"\n",
-    "client_ip": "127.0.0.1"
+    "gamevar": "ANODisabledRegions,string,\"ALL\""
   };
 
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.setHeader('Server', 'nginx/1.20.1');
-  res.setHeader('X-Powered-By', 'PHP/7.4.33');
   res.status(200).json(responseData);
 }
