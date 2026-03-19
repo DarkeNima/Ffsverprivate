7fc0f68322e604a82fe8137d57d61bcf5b127aeb
DarkeNima <naviyaoficial@gmail.com>
2026-03-19 10:35:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ export default {
     const url = new URL(request.url);
     const userAgent = request.headers.get("User-Agent") || "";
 
-    // 1. Browser එකකින් ආවොත් 403 පෙන්වන්න (අර Private Server එකේ වගේමයි)
+    // Browser එකට 403 පෙන්වන කොටස
     if (userAgent.includes("Mozilla") && !userAgent.includes("Dalvik") && !userAgent.includes("Garena")) {
       return new Response("<html><head><title>403 Forbidden</title></head><body><center><h1>403 Forbidden</h1></center><hr><center>nginx/1.20.1</center></body></html>", {
         status: 403,
@@ -11,7 +11,7 @@ export default {
       });
     }
 
-    // 2. ගේම් එකෙන් එන ඕනෑම Request එකකට (404 නොවී) මේ JSON එක දෙන්න
+    // ගේම් එකට දෙන නියම JSON එක (Strict Formatting)
     const responseData = {
       "is_server_open": true,
       "code": 2,
@@ -19,21 +19,22 @@ export default {
       "server_url": "https://mute-firefly-5b34.nimsarahhnaviduhj.workers.dev/",
       "latest_release_version": "1.222.1",
       "remote_version": "1.222.1",
-      "cdn_url": "",
+      "cdn_url": "https://mute-firefly-5b34.nimsarahhnaviduhj.workers.dev/",
       "appstore_url": "http://www.freefiremobile.com/",
       "is_firewall_open": false,
       "garena_hint": false,
       "garena_login": false,
       "is_review_server": false,
       "need_track_hotupdate": false,
-      "gamevar": "var_name,comment,var_type,var_value\nvar_name,comment,\"var_type float, int, bool\",var_value\nANODisabledRegions,\u5173\u95edMTP\u7684\u5730\u533a,string,\"IND,NA\"\nANODisabledClientVariant,ANODisabledClientVariant,string,\"ClientUsingVersion_MAX_HPE,ClientUsingVersion_FFI,ClientUsingVersion_MAX|IND,ClientUsingVersion_MAX|NA,ClientUsingVersion_NORMAL|NA\"\nEnableMtpLiteDataRegion,mtp\u8f7b\u7279\u5f81\u5f00\u5173,string,\"BR,EUROPE,ID,ME,US,RU,SAC,SG,TH,TW,VN,PK,ZA,BD\"\nANOEmulatorCheckDisbaledClientVariant,ANOEmulatorCheckDisbaledClientVariant,string,\"ClientUsingVersion_FFI,ClientUsingVersion_MAX,ClientUsingVersion_NORMAL\"\nForceTutorial_ChangeHudABTest,fps\u6d41\u7a0b\u4e2d\u6253\u5f00hud\u9009\u62e9\u754c\u9762\u7684\u6982\u7387,float,-1\n",
+      "gamevar": "var_name,comment,var_type,var_value\nANODisabledRegions,MTP,string,\"IND,NA\"\n",
       "client_ip": "127.0.0.1"
     };
 
     return new Response(JSON.stringify(responseData), {
       headers: { 
-        "content-type": "application/json;charset=UTF-8",
-        "Access-Control-Allow-Origin": "*" 
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+        "Connection": "keep-alive"
       },
     });
   },
