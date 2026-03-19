e9d66e54c770fde162d5cfb6f62f82bf599a28f3
DarkeNima <naviyaoficial@gmail.com>
2026-03-19 12:40:07 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,27 +1,29 @@
 export default function handler(req, res) {
+  // සර්වර් එකේ response එක හරියටම අර වැඩ කරන එක වගේම හදමු
   const responseData = {
     "is_server_open": true,
     "code": 2,
     "country_code": "IN",
-    "server_url": "https://ffsverprivate.vercel.app/",
-    "latest_release_version": "1.103.1", 
-    "remote_version": "1.103.1",
-    "cdn_url": "https://ffsverprivate.vercel.app/",
+    "server_url": "https://ffsverprivate.vercel.app/", // මෙතනට ඔයාගේ URL එක
+    "latest_release_version": "",
+    "remote_version": "",
+    "cdn_url": "",
     "appstore_url": "http://www.freefiremobile.com/",
     "is_firewall_open": false,
     "garena_hint": false,
     "garena_login": false,
     "is_review_server": false,
     "need_track_hotupdate": false,
-    "gamevar": "ANODisabledRegions,string,\"ALL\"",
+    "gamevar": "var_name,comment,var_type,var_value\nANODisabledRegions,MTP,string,\"IND,NA\"\n",
     "client_ip": "127.0.0.1"
   };
 
-  // Headers ටික අර වැඩ කරන සර්වර් එකේ වගේම හදමු
+  // Headers ටික අනිවාර්යයෙන්ම මේ විදිහටම තියෙන්න ඕනේ
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.setHeader('Server', 'nginx/1.20.1');
   res.setHeader('X-Powered-By', 'PHP/7.4.33');
-  res.setHeader('Connection', 'keep-alive');
-  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+
   return res.status(200).json(responseData);
 }
