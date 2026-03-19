7a2ae5c71817d833316d6442bbbae2932bf20e14
DarkeNima <naviyaoficial@gmail.com>
2026-03-19 11:35:07 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,50 +1,27 @@
-export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    const userAgent = request.headers.get("User-Agent") || "";
+export default function handler(req, res) {
+  // ගේම් එකෙන් එන විස්තර සර්වර් එකේ ලොග් කරනවා
+  console.log("--- New Request Received ---");
+  console.log("Method:", req.method);
+  console.log("Path:", req.url);
+  console.log("User-Agent:", req.headers['user-agent']);
+  console.log("Headers:", JSON.stringify(req.headers));
 
-    // Browser එකට 403 පෙන්වන කොටස (Nginx Style)
-    if (userAgent.includes("Mozilla") && !userAgent.includes("Dalvik") && !userAgent.includes("Garena")) {
-      return new Response("<html><head><title>403 Forbidden</title></head><body><center><h1>403 Forbidden</h1></center><hr><center>nginx/1.20.1</center></body></html>", {
-        status: 403,
-        headers: { 
-          "Content-Type": "text/html",
-          "Server": "nginx/1.20.1" // මෙතනත් අපි Nginx විදිහටම පෙන්වනවා
-        },
-      });
-    }
+  const responseData = {
+    "is_server_open": true,
+    "code": 2,
+    "country_code": "IN",
+    "server_url": "https://ffsverprivate.vercel.app/",
+    "latest_release_version": "",
+    "remote_version": "",
+    "cdn_url": "",
+    "appstore_url": "http://www.freefiremobile.com/",
+    "is_firewall_open": false,
+    "gamevar": "var_name,comment,var_type,var_value\nANODisabledRegions,MTP,string,\"ALL\"\n",
+    "client_ip": "127.0.0.1"
+  };
 
-    // ගේම් එක රවට්ටන නියම JSON එක
-    const responseData = {
-      "is_server_open": true,
-      "code": 2,
-      "country_code": "IN",
-      "server_url": "https://ffsverprivate.vercel.app/",
-      "latest_release_version": "",  // මේවා හිස්ව තියන්න
-      "remote_version": "",          // මේවා හිස්ව තියන්න
-      "cdn_url": "",
-      "appstore_url": "http://www.freefiremobile.com/",
-      "is_firewall_open": false,
-      "garena_hint": false,
-      "garena_login": false,
-      "is_review_server": false,
-      "need_track_hotupdate": false,
-      "gamevar": "var_name,comment,var_type,var_value\nvar_name,comment,\"var_type float, int, bool\",var_value\nANODisabledRegions,关闭MTP的地区,string,\"IND,NA\"\nANODisabledClientVariant,ANODisabledClientVariant,string,\"ClientUsingVersion_MAX_HPE,ClientUsingVersion_FFI,ClientUsingVersion_MAX|IND,ClientUsingVersion_MAX|NA,ClientUsingVersion_NORMAL|NA\"\nEnableMtpLiteDataRegion,mtp轻特征开关,string,\"BR,EUROPE,ID,ME,US,RU,SAC,SG,TH,TW,VN,PK,ZA,BD\"\nANOEmulatorCheckDisbaledClientVariant,ANOEmulatorCheckDisbaledClientVariant,string,\"ClientUsingVersion_FFI,ClientUsingVersion_MAX,ClientUsingVersion_NORMAL\"\nForceTutorial_ChangeHudABTest,fps流程中打开hud选择界面的概率,float,-1\n",
-      "client_ip": "127.0.0.1"
-    };
-
-    // Nginx Headers එක්කම ගේම් එකට Response එක යැවීම
-    return new Response(JSON.stringify(responseData), {
-      status: 200,
-      headers: { 
-        "Content-Type": "application/json; charset=utf-8",
-        "Access-Control-Allow-Origin": "*",
-        "Server": "nginx/1.20.1",          // ගේම් එකට පෙන්වන්නේ Nginx සර්වර් එකක් කියලා
-        "X-Powered-By": "PHP/7.4.33",      // තවත් රියල් කරලා පෙන්වන්න
-        "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0", // පරණ දත්ත ගන්න එක නවත්තන්න
-        "Pragma": "no-cache",
-        "Connection": "keep-alive"
-      },
-    });
-  },
-};
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Server', 'nginx/1.20.1');
+  res.setHeader('X-Powered-By', 'PHP/7.4.33');
+  res.status(200).json(responseData);
+}
