e7dd017e8a64994bb35a0d640961819c202aa6c4
DarkeNima <naviyaoficial@gmail.com>
2026-03-19 10:08:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    if (url.pathname.includes("/live/ver.php")) {
+    // ඕනෑම .php ඉල්ලීමකට (ver.php, client_ver.php ආදී)
+    if (url.pathname.endsWith(".php")) {
       const responseData = {
         "is_server_open": true,
         "code": 2,
-        "country_code": "SL",
+        "country_code": "SG", // Singapore වලට වෙනස් කළා (Garena විදිහටම)
         "server_url": "https://mute-firefly-5b34.nimsarahhnaviduhj.workers.dev/",
-        "latest_release_version": "1.222.1", // මෙතනට ඇත්තම වර්ෂන් එක දෙන්න
-        "remote_version": "1.222.1",         // මෙතනටත් ඒකම දෙන්න
+        "latest_release_version": "1.222.1",
+        "remote_version": "1.222.1",
         "is_firewall_open": false,
         "garena_hint": false,
         "need_track_hotupdate": false,
-        "gamevar": "ANODisabledRegions,string,\"ALL\""
+        "gamevar": "ANODisabledRegions,string,\"NONE\"" // මේක "NONE" කළා
       };
 
       return new Response(JSON.stringify(responseData), {
-        headers: { "content-type": "application/json;charset=UTF-8" },
+        headers: { 
+          "content-type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*" 
+        },
       });
     }
 
-    return new Response(JSON.stringify({ "status": "success" }), {
+    return new Response(JSON.stringify({ "status": "online" }), {
       headers: { "content-type": "application/json" },
     });
   },
