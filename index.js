ae3ba3a7fe3fdd1b18ca9db1a3ba8c2b1ed5fabe
DarkeNima <naviyaoficial@gmail.com>
2026-03-19 10:04:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    // ගේම් එකෙන් එන Request එක අල්ලගන්න තැන
     if (url.pathname.includes("/live/ver.php")) {
       const responseData = {
         "is_server_open": true,
         "code": 2,
         "country_code": "SL",
         "server_url": "https://mute-firefly-5b34.nimsarahhnaviduhj.workers.dev/",
-        "latest_release_version": "1.104.1", 
-        "remote_version": "1.104.1",
+        "latest_release_version": "1.222.1", // මෙතනට ඇත්තම වර්ෂන් එක දෙන්න
+        "remote_version": "1.222.1",         // මෙතනටත් ඒකම දෙන්න
         "is_firewall_open": false,
         "garena_hint": false,
         "need_track_hotupdate": false,
@@ -22,8 +21,7 @@ export default {
       });
     }
 
-    // වෙනත් ඕනෑම Request එකක් ආවොත් (Default)
-    return new Response(JSON.stringify({ "status": "success", "message": "Server is Running via GitHub" }), {
+    return new Response(JSON.stringify({ "status": "success" }), {
       headers: { "content-type": "application/json" },
     });
   },
